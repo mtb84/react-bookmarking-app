e5dc4ccfcb5f33937ad7b76f2b11254e0f8aeff4
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-06-29 22:50:47 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import BookmarkForm from './components/BookmarkForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const URLS = [{
+  url: "http://google.com",
+  title: "Google",
+  tag: "Search Tags"
+},
+{
+  url: "http://github.com",
+  title: `GitHub`,
+  tag: `Post Tags`
+}]
 
-export default App;
+export default function App() {
+
+    const [bookmarks, setBookmarks] = useState(URLS);
+    const [filter, setFilter] = useState(null);
+
+    const addBookmark = (bookmark) => {
+        setBookmarks([...bookmarks, bookmark]);
+    }
+
+    const tags = [...new Set(bookmarks.map(bookmark => bookmark.tag))];
+    
+    const urlTags = tags.map((tag) => (
+        <button key={tag} type="button" value={tag} onClick={() => setFilter(tag)}>{tag}</button>
+    ));
+
+    const filteredBookmarks = bookmarks
+    .filter(bookmark => filter ? bookmark.tag === filter : bookmark)
+    .map((bookmark, index) => (
+        <div key={index} className="url-list-container">
+            <p>{bookmark.title}</p>
+            <a href={bookmark.url}>{bookmark.url}</a>
+        </div>
+    ));
+
+    return(
+        <div class="container">
+            <div><BookmarkForm addBookmark={addBookmark} />
+            <button type="button" value="All" onClick={() => setFilter(null)}>All Tags</button>
+            {urlTags}
+            </div>
+            <div>{filteredBookmarks}</div>
+        </div>
+    )
+}
